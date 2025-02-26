8aa1d6af0b93d9a71b5417f307226f1bbe9fff46
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-02-26 15:00:15 -0300

    refactor: atualiza comentários

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,24 +10,26 @@ function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true); // Criado pois a página estava quebrando por não conseguir carregar as informações do usuário antes da renderização
 
+  // Hook useEffect que chama a função getUserInfo
   useEffect(() => {
     getUserInfo();
   }, []);
 
+  // Função getUserInfo, que retorna os dados do usuário solicitados na Api e atualiza setCurrentUser
   function getUserInfo() {
     api
       .getUserInfo()
       .then((userInfo) => {
-        setCurrentUser(userInfo);
-        setIsLoading(false);
+        setCurrentUser(userInfo); // Atualiza os dados do usuário
+        setIsLoading(false); // Muda o useState do isLoading para false, para renderização da página
       })
       .catch((err) => {
         console.error("Erro", err); //  // Quando popup de erro estiver configurado, colocar aqui
-        setIsLoading(false);
+        setIsLoading(false); // Muda o useStante do isLoading para false, mesmo se der erro
       });
   }
 
-  // Enquanto estiver carregando, exibe essa mensagem na página
+  // Enquanto estiver carregando, exibe essa mensagem na página (melhorar estilização do loading)
   if (isLoading) {
     return <p>Carregando...</p>;
   }
