e391bd5a80c23f40898eba7a538167a464c2f46e
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-03-18 10:44:36 -0300

    refactor: refatoração do componente App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,91 +1,65 @@
 import { useState, useEffect } from "react";
-import "../index.css";
+
+import avatarPlaceholder from "../images/profile__placeholder.png";
+
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
-import Loading from "./Loading/Loading";
-import { api } from "../utils/api.js";
-import CurrentUserContext from "../contexts/CurrentUserContext.js";
-import DeleteConfirmationPopup from "./Main/components/Popup/componentes/DeleteConfirmationPopup/DeleteConfirmationPopup";
+
+import { api } from "../utils/api";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [cards, setCards] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [popup, setPopup] = useState(null);
+  const [cards, setCards] = useState([]);
   const [error, setError] = useState(null);
-  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
-  const [cardToDelete, setCardToDelete] = useState(null);
+  const [currentUser, setCurrentUser] = useState({
+    avatar: avatarPlaceholder,
+    name: "Carregando...",
+    about: "Carregando...",
+  });
+  const [saving, setSaving] = useState(false);
 
-  // Hook useEffect que chama a função getUserInfo
   useEffect(() => {
-    getUserInfo();
-  }, []);
-
-  // Hook useEffect que chama a função getCardsFromApi
-  useEffect(() => {
-    getCardsFromApi();
-  }, []);
-
-  // Função getUserInfo, que retorna os dados do usuário solicitados na Api e atualiza setCurrentUser
-  function getUserInfo() {
     api
       .getUserInfo()
-      .then((userInfo) => {
-        setCurrentUser(userInfo);
-        setIsLoading(false);
+      .then((currentUser) => {
+        setCurrentUser(currentUser);
       })
       .catch((err) => {
-        setError(err); // Define o erro
-        setIsLoading(false);
+        setError(err.message || "Erro ao carregar informações do usuário");
       });
-  }
+  }, []);
 
-  // Função getCardsFromApi, que retorna os Cards solicitados na Api e atualiza setCards
-  function getCardsFromApi() {
+  useEffect(() => {
     api
       .getCardsInfo()
-      .then((cards) => setCards(cards))
+      .then((cards) => {
+        setCards(cards);
+      })
       .catch((err) => {
-        setError(err); // Define o erro
+        setError(err.message || "Erro ao carregar cards");
       });
+  }, []);
+
+  function handleOpenPopup(popup) {
+    setSaving(false);
+    setPopup(popup);
+    setError(null);
   }
 
-  // Atualiza os dados do usuário
-  const handleUpdateUser = (data) => {
-    (async () => {
-      await api
-        .setUserInfo(data.name, data.about)
-        .then((newData) => {
-          setCurrentUser(newData);
-          handleClosePopup();
-        })
-        .catch((err) => {
-          setError(err); // Define o erro
-        });
-    })();
-  };
+  function handleClosePopup() {
+    setPopup(null);
+    setError(null);
+  }
 
-  // Atualiza o avatar do usuário
-  const handleUpdateAvatar = (data) => {
-    (async () => {
-      await api
-        .changeProfileImage(data)
-        .then((newData) => {
-          setCurrentUser(newData);
-          handleClosePopup();
-        })
-        .catch((err) => {
-          setError(err); // Define o erro
-        });
-    })();
-  };
-
-  // Função que atualiza o estado de curtida do card ao clicar no botão like
   async function handleCardLike(card) {
+    // Verificar mais uma vez se esse cartão já foi curtido
     const isLiked = card.isLiked;
+
+    // Enviar uma solicitação para a API e obter os dados do cartão atualizados
     await api
-      .updateLikeState(card._id, !isLiked)
+      .updateLikeState(card._id, isLiked)
       .then((newCard) => {
         setCards((state) =>
           state.map((currentCard) =>
@@ -93,96 +67,88 @@ function App() {
           )
         );
       })
-      .catch((err) => {
-        setError(err); // Define o erro
-      });
+      .catch((err) => setError(err));
   }
 
-  // Função para abrir o popup de confirmação de exclusão
-  const handleOpenDeletePopup = (card) => {
-    setCardToDelete(card); // Armazena o card selecionado para exclusão
-    setIsDeletePopupOpen(true);
-  };
-
-  // Função para fechar o popup de confirmação de exclusão
-  const handleCloseDeletePopup = () => {
-    setIsDeletePopupOpen(false);
-    setCardToDelete(null); // Limpa o card selecionado
-  };
-
-  // Função que remove o card ao clicar no botão excluir
-  async function handleConfirmDelete(card) {
+  async function handleCardDelete(card) {
+    setSaving(true);
     await api
       .deleteCard(card._id)
       .then(() => {
         setCards((state) =>
           state.filter((currentCard) => currentCard._id !== card._id)
         );
+        setPopup(null);
       })
-      .catch((err) => {
-        setError(err); // Define o erro
-      })
-      .finally(() => handleCloseDeletePopup());
+      .catch((err) => setError(err));
   }
 
-  // Função para adicionar um novo card
-  async function handleAddPlaceSubmit(cardData) {
-    await api
-      .addNewCard(cardData.name, cardData.link)
-      .then((newCard) => {
-        setCards([newCard, ...cards]);
-        handleClosePopup();
-      })
-      .catch((err) => {
-        setError(err); // Define o erro
-      });
-  }
+  const handleUpdateUser = (data) => {
+    setSaving(true);
+    (async () => {
+      await api
+        .setUserInfo(data.name, data.about)
+        .then((newData) => {
+          setCurrentUser(newData);
+          setPopup(null);
+        })
+        .catch((err) => {
+          setError(err);
+        });
+    })();
+  };
 
-  // Função para abrir os popups
-  function handleOpenPopup(popup) {
-    setPopup(popup);
-  }
+  const onUpdateAvatar = (data) => {
+    setSaving(true);
+    (async () => {
+      await api
+        .changeProfileImage(data)
+        .then((newData) => {
+          setCurrentUser(newData);
+          setPopup(null);
+        })
+        .catch((err) => {
+          setError(err);
+        });
+    })();
+  };
 
-  // Função para fechar os popups
-  function handleClosePopup() {
-    setPopup(null);
-    setError(null);
-  }
-
-  // Enquanto estiver carregando, exibe essa mensagem na página
-  if (isLoading) {
-    return <Loading />;
-  }
+  const handleAddPlaceSubmit = (data) => {
+    setSaving(true);
+    (async () => {
+      await api
+        .addNewCard(data.name, data.link)
+        .then((newCard) => {
+          setCards([newCard, ...cards]);
+          setPopup(null);
+        })
+        .catch((err) => {
+          setError(err);
+        });
+    })();
+  };
 
   return (
-    <>
-      <CurrentUserContext.Provider
-        value={{ currentUser, handleUpdateUser, handleUpdateAvatar }}
-      >
-        <div className="body">
-          <div className="page">
-            <Header />
-            <Main
-              cards={cards}
-              onCardLike={handleCardLike}
-              onDeleteCard={handleOpenDeletePopup}
-              onAddPlace={handleAddPlaceSubmit}
-              onOpenPopup={handleOpenPopup}
-              onClosePopup={handleClosePopup}
-              popup={popup}
-              err={error} // Passa o erro para o Main
-            />
-            <Footer />
-          </div>
-        </div>
-        {isDeletePopupOpen && (
-          <DeleteConfirmationPopup
-            onClose={handleCloseDeletePopup}
-            onConfirm={() => handleConfirmDelete(cardToDelete)}
+    <CurrentUserContext.Provider
+      value={{ currentUser, handleUpdateUser, onUpdateAvatar, saving }}
+    >
+      <div className="body">
+        <div className="page">
+          <Header />
+          <Main
+            onOpenPopup={handleOpenPopup}
+            onClosePopup={handleClosePopup}
+            popup={popup}
+            cards={cards}
+            onCardLike={handleCardLike}
+            onCardDelete={handleCardDelete}
+            onAddPlaceSubmit={handleAddPlaceSubmit}
+            error={error}
           />
-        )}
-      </CurrentUserContext.Provider>
-    </>
+          <Footer />
+        </div>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
