5351098dc6dac6f9424cfa345677e595c88e7c88
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-03-18 10:14:25 -0300

    refactor: refatoração do componente Card

--- src/components/Main/components/Card/Card.jsx
+++ src/components/Main/components/Card/Card.jsx
@@ -1,61 +1,63 @@
-import deleteButton from "../../../../images/delete__icon.svg";
-import likeButton from "../../../../images/like__icon.svg";
-import ImagePopup from "../Popup/componentes/ImagePopup/ImagePopup";
+import deleteIcon from "../../../../images/delete__icon.svg";
+import likeIcon from "../../../../images/like__icon.svg";
 
-export default function Card({ card, onClick, onCardLike, onDeleteCard }) {
-  const { name, link, isLiked } = card;
+import ImagePopup from "../Popup/components/ImagePopup/ImagePopup";
+import DeleteConfirmation from "../Popup/components/DeleteConfirmation/DeleteConfirmation";
 
-  // Constante que renderiza o popup de imagem
+export default function Card(props) {
+  const { card, onClick, onCardLike, onCardDelete, saving } = props;
+
   const imagePopup = {
-    title: "",
     children: <ImagePopup card={card} />,
   };
 
-  // Verificar se o usuário atual “curtiu” o cartão
+  const deleteConfirmationPopup = {
+    title: "Tem certeza?",
+    children: (
+      <DeleteConfirmation
+        onCardDelete={onCardDelete}
+        card={card}
+        saving={saving}
+      />
+    ),
+  };
+
   const cardLikeButtonClassName = `grid__like-icon ${
-    isLiked ? "grid__like-icon_active" : ""
+    card.isLiked ? "grid__like-icon_active" : ""
   }`;
 
-  // Criação do manipulador de clique
   function handleLikeClick() {
     onCardLike(card);
-  }
-
-  // Criação do removedor de cards
-  function handleDeleteClick() {
-    onDeleteCard(card); // Chama a função passada pelo componente pai para abrir o popup
   }
 
   return (
     <li className="grid__card">
       <img
         className="grid__img"
-        src={link}
-        alt=""
+        src={card.link}
+        alt={`Imagem de ${card.name}`}
         onClick={() => onClick(imagePopup)}
       />
       <button
-        onClick={handleDeleteClick}
         className="grid__card-delete-button"
-        aria-label="Delete card"
-        type="button"
+        onClick={() => onClick(deleteConfirmationPopup)}
       >
         <img
-          src={deleteButton}
+          src={deleteIcon}
           alt="Ícone de Deletar"
           className="grid__delete-icon"
         />
       </button>
       <div className="grid__card-footer">
-        <h2 className="grid__card-title">{name}</h2>
+        <h2 className="grid__card-title">{card.name}</h2>
         <button
-          onClick={handleLikeClick}
           className="grid__card-footer-button"
-          aria-label="Like card"
-          type="button"
+          onClick={() => {
+            handleLikeClick(card);
+          }}
         >
           <img
-            src={likeButton}
+            src={likeIcon}
             alt="Ícone de Curtir"
             className={cardLikeButtonClassName}
           />
